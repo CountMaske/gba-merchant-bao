f7bc2dccb610f08d467043fc78183974189742ca
Count <2671979643@qq.com>
2026-05-15 20:45:57 +0800

    再来一单功能代码开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -50,6 +50,14 @@ public interface OrderService {
      */
     void userCancelById(Long id) throws Exception;
 
+    /**
+     * 再来一单
+     *
+     * @param id
+     */
+    void repetition(Long id);
+
+
 
 
     /**
